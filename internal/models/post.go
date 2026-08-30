91395c735c7fffa14c6865265991eb15cf163b59
surajit <surajitjana20107@gmail.com>
2026-08-31 02:00:38 +0530

    Add Post model with fields for ID, author, title, slug, content, status, visibility, and timestamps

---
+++ internal/models/post.go
@@ -0,0 +1,25 @@
+package models
+
+import (
+	"time"
+
+	"github.com/google/uuid"
+)
+
+type Post struct {
+	ID            uuid.UUID  `gorm:"type:uuid;default:uuid_generate_v4();primary_key" json:"id"`
+	AuthorID      uuid.UUID  `gorm:"type:uuid;not null" json:"author_id"`
+	Author        User       `gorm:"foreignKey:AuthorID" json:"author"`
+	Title         string     `gorm:"not null" json:"title"`
+	Slug          string     `gorm:"unique;not null" json:"slug"`
+	Excerpt       string     `json:"excerpt"`
+	Content       string     `gorm:"type:text" json:"content"`
+	Status        string     `gorm:"default:draft" json:"status"`
+	Visibility    string     `gorm:"default:public" json:"visibility"`
+	PublishedAt   *time.Time `json:"published_at"`
+	Views         int64      `gorm:"defult:0" json:"views"`
+	CommentCount  int64      `gorm:"defult:0" json:"comment_count"`
+	ReactionCount int64      `gorm:"defult:0" json:"reaction_count"`
+	CreatedAt     time.Time  `json:"created_at"`
+	UpdatedAt     time.Time  `json:"updated_at"`
+}
