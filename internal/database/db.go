53dfda14d48da42781e5e57b8c5bf4921e50eaae
surajit <surajitjana20107@gmail.com>
2026-08-31 03:13:50 +0530

    Add database connection and migration logic

---
+++ internal/database/db.go
@@ -0,0 +1,34 @@
+package database
+
+import (
+	"fmt"
+
+	"github.com/surajit/blog-project/config"
+	"github.com/surajit/blog-project/internal/models"
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+)
+
+func Connect(cfg, *config.Config) (*gorm.DB, error) {
+	dsn := fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=%s sslmode=disable", cfg.DbHost, cfg.DBUser, cfg.DBPassword, cfg.DbName, cfg.DbPort)
+
+	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
+	if err != nil {
+		return nil, fmt.Errorf("failed to connect database: %w", err)
+	}
+
+	err = db.AutoMigrate(
+		&models.Comment{},
+		&models.Post{},
+		&models.Reaction{},
+		&models.Tag{},
+		&models.User{},
+	)
+
+	if err != nil {
+		return nil, fmt.Errorf("failed to migrate database: %w", err)
+	}
+
+	fmt.Println("Database connected successfully")
+	return db, nil
+}
