dfd70d96ea855665f50ad629aff4c764cd43ca7b
surajit <surajitjana20107@gmail.com>
2026-08-31 02:13:04 +0530

    Add Comment model with fields for ID, PostID, UserID, ParentID, content, approval status, and timestamps

---
+++ internal/models/comment.go
@@ -0,0 +1,18 @@
+package models
+
+import (
+	"time"
+
+	"github.com/google/uuid"
+)
+
+type Comment struct {
+	ID         uuid.UUID  `gorm:"type:uuid;default:uuid_generate_v4();primary_key" json:"id"`
+	PostID     uuid.UUID  `gorm:"type:uuid;not null" json:"post_id"`
+	UserID     *uuid.UUID `gorm:"type:uuid;not null" json:"user_id"`
+	ParentID   *uuid.UUID `gorm:"type:uuid" json:"parent_uuid"`
+	Content    string     `gorm:"type:text" json:"content"`
+	IsApproved bool       `gorm:"default:false" json:"is_approved"`
+	CreatedAt  time.Time  `json:"created_at"`
+	UpdatedAt  time.Time  `json:"updated_at"`
+}
