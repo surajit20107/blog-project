b8707f829c08591698b0c8dd44a29cadc0a88224
surajit <surajitjana20107@gmail.com>
2026-09-01 22:02:34 +0530

    Implement user creation logic in UserService and add Create method in UserRepository

--- internal/repositories/user_repository.go
+++ internal/repositories/user_repository.go
@@ -1,6 +1,9 @@
 package repositories
 
-import "gorm.io/gorm"
+import (
+	"github.com/surajit/blog-project/internal/models"
+	"gorm.io/gorm"
+)
 
 type UserRepository struct{
 	db *gorm.DB
@@ -10,4 +13,8 @@ func NewUserRepository(db *gorm.DB) *UserRepository {
 	return &UserRepository{
 		db: db,
 	}
+}
+
+func (r *UserRepository) Create(user *models.User) error {
+	return r.db.Create(user).Error
 }
--- internal/services/user_service.go
+++ internal/services/user_service.go
@@ -1,6 +1,13 @@
 package services
 
-import "github.com/surajit/blog-project/internal/repositories"
+import (
+	"time"
+
+	"github.com/google/uuid"
+	"github.com/surajit/blog-project/internal/auth"
+	"github.com/surajit/blog-project/internal/models"
+	"github.com/surajit/blog-project/internal/repositories"
+)
 
 type UserService struct{
 	repo *repositories.UserRepository
@@ -10,4 +17,26 @@ func NewUserService(r *repositories.UserRepository) *UserService {
 	return &UserService{
 		repo: r,
 	}
+}
+
+func (s *UserService) Create(username, email, password string) (*models.User, error) {
+	u := models.User{
+		ID: uuid.New(),
+		UserName: username,
+		Email: email,
+		Password: password,
+		DisplayName: username,
+		Role: "reader",
+		CreatedAt: time.Now(),
+		UpdatedAt: time.Now(),
+	}
+	hashedPassword, err := auth.HashedPassword(password)
+	if err != nil {
+		return nil, err
+	}
+	u.Password = hashedPassword
+	if err := s.repo.Create(&u); err != nil {
+		return nil, err
+	}
+	return &u, nil
 }
