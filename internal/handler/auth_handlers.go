8932fb04de9ae24d673d742e5cdd692f873e5ddf
Surajit <surajitjana20107@gmail.com>
2026-09-02 19:17:49 +0530

    Fix capitalization in success messages and implement Login functionality in AuthHandler

--- internal/handler/auth_handlers.go
+++ internal/handler/auth_handlers.go
@@ -34,5 +34,25 @@ func (h *AuthHandler) SignUp(c echo.Context) error {
 	if err != nil {
 		return utils.Err(c, http.StatusBadRequest, err.Error())
 	}
-	return utils.JSON(c, http.StatusCreated, true, "user created successfully!", u)
+	return utils.JSON(c, http.StatusCreated, true, "User Created Successfully!", u)
+}
+
+type LoginReq struct {
+	Email    string `json:"email,omitempty"`
+	Password string `json:"password,omitempty"`
+}
+
+func (h *AuthHandler) Login(c echo.Context) error {
+	var req LoginReq
+	if err := c.Bind(&req); err != nil {
+		return utils.Err(c, http.StatusBadRequest, "Invalid Payload")
+	}
+	u, token, err := h.services.Login(req.Email, req.Password)
+	if err != nil {
+		return utils.Err(c, http.StatusBadRequest, "Invalid Credentials!")
+	}
+	return utils.JSON(c, http.StatusOK, true, "Login Successfully!", map[string]interface{}{
+		"user":  u,
+		"token": token,
+	})
 }
