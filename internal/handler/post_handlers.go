8d9101821089d5ee0488e6cc20dd198ff7a80ac7
Surajit <surajitjana20107@gmail.com>
2026-09-02 19:48:27 +0530

    Add PostHandler and NewPostHandler for post service integration

--- internal/handler/post_handlers.go
+++ internal/handler/post_handlers.go
@@ -1,1 +1,13 @@
 package handler
+
+import "github.com/surajit/blog-project/internal/services"
+
+type PostHandler struct {
+	Service *services.PostService
+}
+
+func NewPostHandler(s *services.PostService) *PostHandler {
+	return &PostHandler{
+		Service: s,
+	}
+}
