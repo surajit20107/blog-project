6c5ba10bd18bcffa7008e48822ab753a3a016f64
Surajit <surajitjana20107@gmail.com>
2026-09-02 11:32:15 +0530

    Implement Create method in PostService for creating posts with tags

--- internal/services/post_service.go
+++ internal/services/post_service.go
@@ -1,18 +1,63 @@
 package services
 
 import (
+	"github.com/google/uuid"
+	"github.com/surajit/blog-project/internal/models"
 	"github.com/surajit/blog-project/internal/repositories"
+	"github.com/surajit/blog-project/internal/utils"
 	"gorm.io/gorm"
+	"time"
 )
 
-type PostService struct{
+type PostService struct {
 	repo *repositories.PostRepository
-	db *gorm.DB
+	db   *gorm.DB
 }
 
 func NewPostService(r *repositories.PostRepository, db *gorm.DB) *PostService {
 	return &PostService{
 		repo: r,
-		db: db,
+		db:   db,
 	}
+}
+
+func (s *PostService) Create(authorId uuid.UUID, title, content string, tags []string) (*models.Post, error) {
+	post := &models.Post{
+		ID:       uuid.New(),
+		AuthorID: authorId,
+		Title:    title,
+		Slug:     utils.GenerateSlug(title + "-" + uuid.NewString()[:6]),
+		Status: "published",
+		Visibility: "public",
+		CreatedAt: time.Now(),
+		UpdatedAt: time.Now(),
+	}
+	err := s.db.Transaction(func(tx *gorm.DB) error {
+		if err := tx.Create(post).Error; err != nil {
+			return err
+		}
+		for _, tagName := range tags {
+			var tag models.Tag
+			if err := tx.Where("name=?", tagName).First(&tag).Error; err != nil {
+				tag = models.Tag{
+					ID:   uuid.New(),
+					Name: tagName,
+					Slug: utils.GenerateSlug(tagName),
+					CreatedAt: time.Now(),
+					UpdatedAt: time.Now(),
+				}
+				if err := tx.Create(&tag).Error; err != nil {
+					return err
+				}
+			}
+			if err := tx.Model(post).Association("Tags").Append(&tag); err != nil {
+				return err
+			}
+		}
+		return nil
+	})
+	if err != nil {
+		return nil, err
+	}
+	return post, nil
 }
