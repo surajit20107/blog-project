20ed75f12a7a56b0dcc20231678dcab6aa566660
Surajit <surajitjana20107@gmail.com>
2026-09-02 19:40:41 +0530

    Implement AddComment functionality in CommentHandler

--- internal/handler/comment_handlers.go
+++ internal/handler/comment_handlers.go
@@ -1,6 +1,12 @@
 package handler
 
-import "github.com/surajit/blog-project/internal/services"
+import (
+	"net/http"
+
+	"github.com/labstack/echo/v4"
+	"github.com/surajit/blog-project/internal/services"
+	"github.com/surajit/blog-project/internal/utils"
+)
 
 type CommentHandler struct {
 	Service *services.CommentService
@@ -10,4 +16,27 @@ func NewCommentHandler(s *services.CommentService) *CommentHandler {
 	return &CommentHandler{
 		Service: s,
 	}
+}
+
+type AddCommentReq struct {
+	Content  string  `json:"content,omitempty"`
+	ParentID *string `json:"parent_id,omitempty"`
+}
+
+func (h *CommentHandler) AddComment(c echo.Context) error {
+	postID := c.Param("id")
+	var req AddCommentReq
+	if err := c.Bind(&req); err != nil {
+		return utils.Err(c, http.StatusBadRequest, "Invalid Payload")
+	}
+	var userID *string
+	if u := c.Get("user_id"); u != nil {
+		uid := u.(string)
+		userID = &uid
+	}
+	comment, err := h.Service.Add(postID, userID, req.ParentID, req.Content)
+	if err != nil {
+		return utils.Err(c, http.StatusInternalServerError, err.Error())
+	}
+	return utils.JSON(c, http.StatusCreated, true, "Comment Added!", comment)
 }
