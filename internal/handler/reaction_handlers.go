b1021d816a79c3eddb94176cca8a6e1be3a924c0
Surajit <surajitjana20107@gmail.com>
2026-09-02 21:20:07 +0530

    Implement ReactionHandler with NewReactionHandler constructor for managing reactions

--- internal/handler/reaction_handlers.go
+++ internal/handler/reaction_handlers.go
@@ -1,1 +1,13 @@
 package handler
+
+import "github.com/surajit/blog-project/internal/services"
+
+type ReactionHandler struct {
+	Service *services.ReactionService
+}
+
+func NewReactionHandler(s *services.ReactionService) *ReactionHandler {
+	return &ReactionHandler{
+		Service: s,
+	}
+}
