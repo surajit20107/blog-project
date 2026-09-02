4da8bd249d3571e335f2c3ebe7569f4be912b8bf
Surajit <surajitjana20107@gmail.com>
2026-09-02 18:23:00 +0530

    Implement Add method in CommentService for creating comments

--- internal/services/comment_service.go
+++ internal/services/comment_service.go
@@ -1,6 +1,10 @@
 package services
 
 import (
+	"time"
+
+	"github.com/google/uuid"
+	"github.com/surajit/blog-project/internal/models"
 	"github.com/surajit/blog-project/internal/repositories"
 	"gorm.io/gorm"
 )
@@ -15,4 +19,36 @@ func NewCommentService(r *repositories.CommentRepository, db *gorm.DB) *CommentS
 		repo: r,
 		db: db,
 	}
+}
+
+func (r *CommentService) Add(postId string, userId *string, parentId *string, content string) (*models.Comment, error) {
+	var post models.Post
+	if err := r.db.First(&post, "id=?", postId).Error; err != nil {
+		return nil, err
+	}
+	// Proceed with the comment creation
+	now := time.Now()
+	comment := &models.Comment{
+		ID: uuid.New(),
+		PostID: uuid.MustParse(postId),
+		Content: content,
+		CreatedAt: now,
+		UpdatedAt: now,
+	}
+	if userId != nil {
+		uid := uuid.MustParse(*userId)
+		comment.UserID = &uid
+	}
+	if parentId != nil && *parentId != "" {
+		var parent models.Comment
+		if err := r.db.First(&parent, "id=?", *parentId).Error; err != nil  {
+			return nil, err
+		}
+		pid := uuid.MustParse(*parentId)
+		comment.ParentID = &pid
+	}
+	if err := r.repo.Create(comment); err != nil {
+		return nil, err
+	}
+	return comment, nil
 }
