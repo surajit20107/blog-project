9e57790ac0de4c8f381d86ec4783b5e02b0ecc03
Surajit <surajitjana20107@gmail.com>
2026-09-02 19:22:14 +0530

    Add CommentHandler and NewCommentHandler for comment service integration

--- internal/handler/comment_handlers.go
+++ internal/handler/comment_handlers.go
@@ -1,1 +1,13 @@
 package handler
+
+import "github.com/surajit/blog-project/internal/services"
+
+type CommentHandler struct {
+	Service *services.CommentService
+}
+
+func NewCommentHandler(s *services.CommentService) *CommentHandler {
+	return &CommentHandler{
+		Service: s,
+	}
+}
