b4590bbbb7c4955640a21edd0495a52c2150aeb4
Surajit <surajitjana20107@gmail.com>
2026-09-02 18:22:48 +0530

    Implement CommentRepository with Create method for adding comments

--- internal/repositories/comment_repositiry.go
+++ internal/repositories/comment_repositiry.go
@@ -1,7 +1,20 @@
 package repositories
 
-import "gorm.io/gorm"
+import (
+	"github.com/surajit/blog-project/internal/models"
+	"gorm.io/gorm"
+)
 
 type CommentRepository struct{
 	db *gorm.DB
+}
+
+func NewCommentRepository(db *gorm.DB) *CommentRepository {
+	return &CommentRepository{
+		db: db,
+	}
+}
+
+func (r *CommentRepository) Create(comment *models.Comment) error {
+	return r.db.Create(comment).Error
 }
