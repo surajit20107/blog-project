d3a5d2a0fee158e4ee74a08b7c5e1dbfa9795382
Surajit <surajitjana20107@gmail.com>
2026-09-02 20:12:42 +0530

    Implement CreatePost functionality in PostHandler for post creation

--- internal/handler/post_handlers.go
+++ internal/handler/post_handlers.go
@@ -1,6 +1,13 @@
 package handler
 
-import "github.com/surajit/blog-project/internal/services"
+import (
+	"net/http"
+
+	"github.com/google/uuid"
+	"github.com/labstack/echo/v4"
+	"github.com/surajit/blog-project/internal/services"
+	"github.com/surajit/blog-project/internal/utils"
+)
 
 type PostHandler struct {
 	Service *services.PostService
@@ -10,4 +17,30 @@ func NewPostHandler(s *services.PostService) *PostHandler {
 	return &PostHandler{
 		Service: s,
 	}
+}
+
+type CreatePostReq struct {
+	Title   string   `json:"title,omitempty"`
+	Content string   `json:"content,omitempty"`
+	Tags    []string `json:"tags,omitempty"`
+}
+
+func (h *PostHandler) CreatePost(c echo.Context) error {
+	var req CreatePostReq
+	if err := c.Bind(&req); err != nil {
+		return utils.Err(c, http.StatusBadRequest, "Invalid Request")
+	}
+	userIDSrt, ok := c.Get("user_id").(string)
+	if !ok {
+		return utils.Err(c, http.StatusUnauthorized, "Invalid User ID") 
+	}
+	authorId, err := uuid.Parse(userIDSrt)
+	if err != nil {
+		return utils.Err(c, http.StatusUnauthorized, "Invalid User ID")
+	}
+	post, err := h.Service.Create(authorId, req.Title, req.Content, req.Tags)
+	if err != nil {
+		return utils.Err(c, http.StatusInternalServerError, err.Error())
+	}
+	return utils.JSON(c, http.StatusCreated, true, "Post Created Successfully!", post)
 }
